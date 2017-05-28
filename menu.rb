bb8736fd725230238919969e51f2f94c17abab9b
eneko <enekosampedro@gmail.com>
2017-05-28 13:02:53 +0200

    Add Menu
    + Add Menu
    + Add Menu sfx
    + Add Menu font
    * Change main window file

---
+++ menu.rb
@@ -0,0 +1,47 @@
+class Menu
+
+  def initialize
+    @imgOption = Gosu::Image.new("img/b3.png")
+    @soundSelect1 = Gosu::Sample.new("sound/select1.wav")
+    @soundSelect2 = Gosu::Sample.new("sound/select2.wav")
+    @option = 1
+  end
+
+  def update
+    if @option > 2
+      @option = 1
+    elsif @option < 1
+      @option = 2
+    end
+  end
+
+  def draw
+    $font.draw("Play", 250, 250, 1, 1.0, 1.0, Gosu::Color::YELLOW)
+    $font.draw("Options", 250, 300, 1, 1.0, 1.0, Gosu::Color::YELLOW)
+
+    if @option == 1
+      @imgOption.draw(225, 255, 1)
+    else
+      @imgOption.draw(225, 305, 1)
+    end
+
+
+    $font.draw("jajasuperman & thadah - 2017", 200, 450, 1, 0.5, 0.5, Gosu::Color::YELLOW)
+  end
+
+  def button_down(id)
+    if id == Gosu::KB_UP
+      @soundSelect1.play()
+      @option += 1
+    elsif id == Gosu::KB_DOWN
+      @soundSelect1.play()
+      @option -= 1
+    end
+
+    if id == Gosu::KB_RETURN
+      @soundSelect2.play()
+      YeePang.changeState(:game)
+    end
+  end
+
+end
--- yeepang.rb
+++ yeepang.rb
@@ -1,140 +1,42 @@
 require 'gosu'
-require 'chipmunk'
 
-require_relative 'player'
-require_relative 'wall'
-require_relative 'harpoon'
-require_relative 'levels'
+require_relative 'menu'
+require_relative 'game'
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-INFINITY = 1.0/0
+
+$font = nil
+$currentState = nil
 
 class YeePang < Gosu::Window
 
-  attr_accessor :space
-
   def initialize
-    super 640, 480
+    super SCREEN_WIDTH, SCREEN_HEIGHT
     self.caption = "YeePang Jokoa"
 
-    @font = Gosu::Font.new(20)
+    $currentState = Menu.new()
 
-    @space = Levels.getSpace()
-
-    @dt = (1.0/60.0)
-
-    @player = Player.new(self)
-
-    @levelNum = 3
-    @balls, @bricks = Levels.level(self, @levelNum)
-
-    @ballToRemove = nil
-
-    @wall0 = Wall.new(self, 13, 13, SCREEN_WIDTH - 13, 0)       # up
-    @wall1 = Wall.new(self, 13, 347 - 13, SCREEN_WIDTH - 13, 0) # down
-    @wall2 = Wall.new(self, 13, 13, 0, 347 - 13)                # left
-    @wall3 = Wall.new(self, SCREEN_WIDTH - 13, 13, 0, 347 - 13) # right
-
-    @harpoon = nil
-    @time = nil
-
-    @back = Gosu::Image.load_tiles("img/back.png", 640, 347)
-    @back2 = Gosu::Image.new("img/back2.png")
-
-    @space.add_collision_func(:player, :ball) do
-      @player.dead = true
-    end
-    @space.add_collision_func(:harpoon, :ball) do |harpoon, ballShape|
-      @ballToRemove = ballShape
-    end
-
+    $font = Gosu::Font.new(self, "./font/ARCADECLASSIC.TTF", 30)
   end
 
   def update
-    if Gosu.button_down? Gosu::KB_RIGHT and (@time == nil or Time.now.to_i > @time + 0.5)
-      @player.move(:right)
-    elsif Gosu.button_down? Gosu::KB_LEFT and (@time == nil or Time.now.to_i > @time + 0.5)
-      @player.move(:left)
-    else
-      @player.move(:stop)
-    end
-
-    10.times do
-      @space.step(@dt)
-    end
-
-    if @player.dead == true
-      @space.remove_body(@player.shape.body)
-      @space.remove_shape(@player.shape)
-      @balls.each do |b|
-        @space.remove_body(b.shape.body)
-        @space.remove_shape(b.shape)
-      end
-    end
-
-    if @harpoon != nil
-      @harpoon.update()
-
-      if @harpoon.shape.body.p.y <= 179
-        @space.remove_body(@harpoon.shape.body)
-        @space.remove_shape(@harpoon.shape)
-        @harpoon = nil
-      end
-
-      if @ballToRemove != nil
-        ball = @balls.detect { |ball| ball.shape == @ballToRemove }
-        @balls.delete(ball)
-        @space.remove_body(ball.shape.body)
-        @space.remove_shape(ball.shape)
-        @ballToRemove = nil
-
-        if(ball.size != 4)
-          @balls.push(Ball.new(self, ball.x - 10, ball.y, ball.size + 1, -1))
-          @balls.push(Ball.new(self, ball.x + 10, ball.y, ball.size + 1, 1))
-        end
-
-        if @harpoon != nil
-          @space.remove_body(@harpoon.shape.body)
-          @space.remove_shape(@harpoon.shape)
-          @harpoon = nil
-        end
-      end
-
-      if @balls.empty?
-        @levelNum += 1
-        @balls, @bricks = Levels.level(self, @levelNum)
-      end
-    end
-
-    if Gosu.button_down? Gosu::KB_SPACE
-      if @harpoon == nil
-        @time = Time.now.to_i
-        @harpoon = Harpoon.new(self, @player.shape.body.p.x)
-        @player.move(:stop)
-      end
-    end
-
-    if Gosu.button_down? Gosu::KB_ESCAPE
-      exit
-    end
+    $currentState.update()
   end
 
   def draw
-    @back[@levelNum-1].draw(0, 0, 1)
-    @back2.draw(13, 0, 3)
-    @player.draw
-
-    @balls.each do |b| b.draw end
-    @bricks.each do |b| b.draw end
-
-    if @harpoon != nil
-      @harpoon.draw()
-    end
-
-    @font.draw("YeePang Jokoa", 10, 10, 0.1, 1.0, 1.0, Gosu::Color::YELLOW)
+    $currentState.draw()
   end
 
+  def button_down(id)
+    $currentState.button_down(id)
+  end
+
+  def self.changeState(state)
+    if state == :game
+      $currentState = Game.new()
+    end
+  end
 end
 
 YeePang.new.show
